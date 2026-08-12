77289e9fed24654ddd2c8f06f3687a1dcc2d2470
¨lautaro <shortlauty35@gmail.com>
2026-08-12 09:24:13 -0300

    se termino el ejersicio 1

--- ejersicio1.ts
+++ ejersicio1.ts
@@ -1,9 +1,47 @@
-class producto {
+class Producto {
   nombre: string;
   precios: number;
   categoria: string;
+  stock: number;
 
-  constructor(nombre: string) {
+  constructor(
+    nombre: string,
+    precios: number,
+    categoria: string,
+    stock: number,
+  ) {
     this.nombre = nombre;
+    this.precios = precios;
+    this.categoria = categoria;
+    this.stock = stock;
+  }
+  describir(): string {
+    return `descripcion del producto: 
+    Nombre: ${this.nombre}
+    Precio: $${this.precios}
+    Categoria: ${this.categoria}
+    Stock: ${this.stock}`;
+  }
+  hayStock(cantidad: number): boolean {
+    if (cantidad > 0 && cantidad <= this.stock) {
+      return true;
+    } else return false;
+  }
+  venderUnidades(cantidad: number, validostock: boolean): string {
+    if (validostock === true) {
+      this.stock -= cantidad;
+      return `ventas total : ${this.stock}`;
+    } else return "no hay suficiente stock para vender";
+  }
+  aplicarDescuento(porcentaje: number): number {
+    let descuento = porcentaje / 100;
+    return this.precios * descuento;
   }
 }
+
+const producto = new Producto("Secadora", 170, "electrodomestico", 5);
+const validostock = producto.hayStock(producto.stock);
+console.log(producto.describir());
+console.log(validostock);
+console.log(producto.venderUnidades(5, validostock));
+console.log(producto.aplicarDescuento(50));
